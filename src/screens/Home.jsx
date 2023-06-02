3fd580c3917cefbe7aa8754e7fc3e32a6d1f4d99
yorch <jorgellave46@gmail.com>
2023-06-02 00:13:41 -0400

    miau

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -13,7 +13,6 @@ const Home = () => {
   const [actionMovies, setActionMovies] = useState([]);
 
   useEffect(() => {
-    // Realiza la llamada a la API para obtener las películas más recientes y próximas
     fetch(
       "https://api.themoviedb.org/3/movie/now_playing?api_key=43d89bad4404d1398e74db3cdffcde40"
     )
@@ -52,7 +51,6 @@ const Home = () => {
       <MovieImages category="Top Movies" movies={topMovie || []}/>
       <MovieImages category="Action Movies" movies={actionMovies || []}/>
       <NavigationBar/>
-      {/* <SeccionMovie/> */}
     </div>
   );
 };
